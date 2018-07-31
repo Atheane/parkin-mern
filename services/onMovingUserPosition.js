6d20508409f4c0eae757f77ab4849a0edea78784
Elodie Royant <eroyant@gmail.com>
2018-07-31 11:10:35 +0200

    notifs for 80m is better

--- services/onMovingUserPosition.js
+++ services/onMovingUserPosition.js
@@ -22,10 +22,11 @@ export default (socket) => {
               const shouldPushANotification = geodist(
                 {lat: userPosition.latitude, lon: userPosition.longitude},
                 {lat: spot.loc.coordinates[1], lon: spot.loc.coordinates[0]},
-                {unit: 'meters', limit: 10}
+                {unit: 'meters', limit: 80}
               )
               let counter = 0
               if (shouldPushANotification && counter === 0) {
+                console.log(counter)
                 counter +=1
                 const title = 'Parkin'
                 const body = 'Vous êtes arrivé'
@@ -63,6 +64,43 @@ export default (socket) => {
                   }
                 }
                 pushNotifications()
+              
+                // sending a notification produces a ticket, which
+                // contains a receipt ID you later use to get the receipt.
+                // The receipts may contain error codes to which you must respond. 
+                let receiptIds = [];
+                for (let ticket of tickets) {
+                  if (ticket.id) {
+                    receiptIds.push(ticket.id);
+                  }
+                }
+
+                let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+                const retrieveErrors = async () => {
+                  for (let chunk of receiptIdChunks) {
+                    try {
+                      let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+                      console.log(receipts);
+
+                      for (let receipt of receipts) {
+                        if (receipt.status === 'ok') {
+                          continue;
+                        } else if (receipt.status === 'error') {
+                          console.error(`There was an error sending a notification: ${receipt.message}`);
+                          if (receipt.details && receipt.details.error) {
+                            // The error codes are listed in the Expo documentation:
+                            // https://docs.expo.io/versions/latest/guides/push-notifications#response-format 
+                            // You must handle the errors appropriately.
+                            console.error(`The error code is ${receipt.details.error}`);
+                          }
+                        }
+                      }
+                    } catch (error) {
+                      console.error(error);
+                    }
+                  }
+                }
+              retrieveErrors()
               }
             } else {
               console.log("onMovingUserPosition, No spot assined to user", user)
